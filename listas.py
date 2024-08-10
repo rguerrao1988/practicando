f09f7dd8da21720b1a501ba00371085aad7d6423
ramon <ramonguerraospino@hotmail.es>
2024-08-10 11:34:09 -0500

    actualizando ando

--- listas.py
+++ listas.py
@@ -7,3 +7,14 @@ print(lista[-2]) #empieza desde el ultimo elemento
 print(lista[0:3])
 print(lista[2:]) # desde el indice 2 al final
 print(lista[-3:])#desde el ultimo indice hasta el -3
+    
+lista[2]= "sabado" #remplazar un elemento de la lista
+print(lista)
+for listas in lista: # imprime uno por uno
+    print(listas)
+
+def listilla(lista):
+    if "sabado" in lista:
+        return 1
+if listilla == 1:
+    print("hola")
