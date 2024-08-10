685ff4c5503ee6abeb32c9a89f4ba6354b414e03
ramon <ramonguerraospino@hotmail.es>
2024-08-10 10:02:17 -0500

    actualizando

--- listas.py
+++ listas.py
@@ -1,7 +1,9 @@
 # lista
+# indice  0        1         2          3        4
 lista=["lunes","martes","miercoles","jueves","viernes"]
 print(lista)
-print(lista[2])
-print(lista[-2])
+print(lista[2]) # desde el indice 0 al 2 --no imprime el ultimo
+print(lista[-2]) #empieza desde el ultimo elemento
 print(lista[0:3])
-print(lista[-1:-3])
+print(lista[2:]) # desde el indice 2 al final
+print(lista[-3:])#desde el ultimo indice hasta el -3
